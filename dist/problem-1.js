44d65bc58446f5cce59062e6c757be396484d9bd
Apurbopaul131 <apurbopaul131@gmail.com>
2024-11-06 23:53:27 +0600

    Solved problem-1

--- dist/problem-1.js
+++ dist/problem-1.js
@@ -1,7 +1,16 @@
 "use strict";
 {
-    //problem-1
-    const str = "hello world";
-    console.log(str);
+    const sumArray = (numArr) => {
+        const totalVaule = numArr.reduce((sum, num) => {
+            return (sum += num);
+        }, 0);
+        return totalVaule;
+    };
+    //Takes two array to check the function
+    const numbers1 = [50, 70, 90, 100, 200];
+    const numbers2 = [700, 200, 500, 400];
+    //call the function and display output
+    console.log(sumArray(numbers1));
+    console.log(sumArray(numbers2));
     //
 }
--- src/problem-1.ts
+++ src/problem-1.ts
@@ -1,6 +1,21 @@
 {
   //problem-1
-  const str: string = "hello world";
-  console.log(str);
+  // Problem Tilte: Write a TypeScript function sumArray that takes an array of numbers and returns the sum of all elements in the array.
+
+  // Create a type to crate input argument array of sumArray function
+  type Array = number[];
+  const sumArray = (numArr: number[]): number => {
+    const totalVaule: number = numArr.reduce((sum, num) => {
+      return (sum += num);
+    }, 0);
+    return totalVaule;
+  };
+  //Takes two array to check the function
+  const numbers1: Array = [50, 70, 90, 100, 200];
+  const numbers2: Array = [700, 200, 500, 400];
+
+  //call the function and display output
+  console.log(sumArray(numbers1));
+  console.log(sumArray(numbers2));
   //
 }
