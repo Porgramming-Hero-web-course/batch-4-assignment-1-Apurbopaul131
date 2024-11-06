f53285023653865bc92ae3e46d8fc4bb5efb4a18
Apurbopaul131 <apurbopaul131@gmail.com>
2024-11-06 23:38:51 +0600

    create assignment structure with tsc compiler and run with node

---
+++ dist/problem-1.js
@@ -0,0 +1,7 @@
+"use strict";
+{
+    //problem-1
+    const str = "hello world";
+    console.log(str);
+    //
+}
---
+++ src/problem-1.ts
@@ -0,0 +1,6 @@
+{
+  //problem-1
+  const str: string = "hello world";
+  console.log(str);
+  //
+}
