32a4427bbf810c225525219c0451a59cea770ac9
Apurbopaul131 <apurbopaul131@gmail.com>
2024-11-07 10:15:31 +0600

    tsc-node-dev installed

--- dist/check.js
+++ dist/check.js
@@ -1,1 +1,15 @@
 "use strict";
+{
+    const updateProfile = (obj, objPro) => {
+        for (let objP in objPro) {
+            for (let ob in obj) {
+                if (objP === ob) {
+                    obj[ob] = objPro[objP];
+                    break;
+                }
+            }
+        }
+        return obj;
+    };
+    console.log(updateProfile({ name: "Apubo", age: 24, email: "apurbopaul131@gmail.com" }, { name: "Antu", age: 25 }));
+}
